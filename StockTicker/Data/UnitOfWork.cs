fef7f872a2b385fd7b4f1004b68e3e929884b866
kcroshaw <kolbycroshaw@gmail.com>
2022-06-27 18:40:06 -0600

    Add project files.

---
+++ StockTicker/Data/UnitOfWork.cs
@@ -0,0 +1,55 @@
+﻿using StockTicker.Interfaces;
+
+namespace StockTicker.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDBContext _dbContext;
+
+        public UnitOfWork(ApplicationDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        //private IGenericRepository<ApplicationUser> _ApplicationUser;
+        //private IGenericRepository<Transaction> _Transaction;
+
+        //public IGenericRepository<ApplicationUser> ApplicationUser
+        //{
+        //    get
+        //    {
+        //        if (_ApplicationUser == null)
+        //        {
+        //            _ApplicationUser = new GenericRepository<ApplicationUser>(_dbContext);
+        //        }
+        //        return _ApplicationUser;
+        //    }
+        //}
+        //public IGenericRepository<Transaction> Transaction
+        //{
+        //    get
+        //    {
+        //        if (_Transaction == null)
+        //        {
+        //            _Transaction = new GenericRepository<Transaction>(_dbContext);
+        //        }
+        //        return _Transaction;
+        //    }
+        //}
+
+        public int Commit()
+        {
+            return _dbContext.SaveChanges();
+        }
+
+        public async Task<int> CommitAsync()
+        {
+            return await _dbContext.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _dbContext.Dispose();
+        }
+    }
+}
---
+++ StockTicker/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using StockTicker.Data;
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDBContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDBContextConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+    options.UseSqlServer(connectionString));;
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDBContext>();;
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();;
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
