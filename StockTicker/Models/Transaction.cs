4053fa4b81378e23b022655f4a04bbf02869430a
kcroshaw <kolbycroshaw@gmail.com>
2022-07-11 18:31:05 -0600

    Got API call working and assigning the data to the Stock object

---
+++ StockTicker/Models/Transaction.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StockTicker.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int Id { get; set; }
+        public string TransactionType { get; set; }
+        public int TransactionAmount { get; set; }
+        public DateTime DateTime { get; set; }
+
+    }
+}
--- StockTicker/Pages/Index.cshtml.cs
+++ StockTicker/Pages/Index.cshtml.cs
@@ -5,6 +5,10 @@ using StockTicker.Data;
 using StockTicker.Models;
 using System;
 
+using ChartDirector;
+using System.Net;
+using Newtonsoft.Json;
+
 namespace StockTicker.Pages
 {
     public class IndexModel : PageModel
@@ -19,10 +23,27 @@ namespace StockTicker.Pages
         private int weekcounter = 0;
         private DateTime tempDate;
 
+        public List<string> listOfStrings = new List<string>();
+
         public IActionResult OnGet()
         {
             if (User.Identity.IsAuthenticated)
             {
+                //Call api and assign response to Stock Object with selected ticker and date
+                var ticker = "AAPL";
+                var date = "2020-10-14";
+                var apiRequest = $"https://api.polygon.io/v1/open-close/{ticker}/{date}?adjusted=true&apiKey=6TH_lUVoIIueeLAJwbCSPncDIEsGQG0d";
+                WebRequest request = WebRequest.Create(apiRequest);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream dataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
+                string responseFromServer = reader.ReadToEnd();
+                Stock DeserializedObject = JsonConvert.DeserializeObject<Stock>(responseFromServer);
+
+                // Cleanup the streams and the response.
+                reader.Close();
+                dataStream.Close();
+                response.Close();
                 return Page();
             }
             else
@@ -32,6 +53,10 @@ namespace StockTicker.Pages
 
         }
 
+        public IActionResult OnPostGetAjax(string name)
+        {
+            return new JsonResult("Hello " + name);
+        }
         //this function should be used to update the data shown on the charts 
         public void ProgressGameplay(DateTime date, string stockSymbol)
         {
@@ -70,7 +95,7 @@ namespace StockTicker.Pages
             return start.AddDays(gen.Next(range));
         }
         
-        public void InitGame()
+        public async void InitGame()
         {
             //put $10,000 dollars into users DB entry
 
@@ -82,54 +107,54 @@ namespace StockTicker.Pages
 
         }
 
-
         public async Task<IActionResult> OnPostGameStart()
         {
-            startArea.style.display = 'none'; //dont know how to directly access html elements from here, everything i saw online said to do this but it doesnt work..... -AG
-            gameArea.style.display = 'block';
-            
-            InitGame();
+            //startArea.style.display = 'none'; //dont know how to directly access html elements from here, everything i saw online said to do this but it doesnt work..... -AG
+            //gameArea.style.display = 'block';
+
+            await Task.Run(() => InitGame());
+            return RedirectToPage("./Index");
 
             //call function that handles gameplay stuff
             //ProgressGameplay(/*pass datetime from the users DB entry*/,/*pass stock symbol from users DB entry*/);
-        
-            
-        }
 
-        public async Task<IActionResult> OnPostBuy()
-        {
-            //have user indicate how much stock to buy
-            //maybe have a seperate div with a input box and button that appears when buy is clicked and make the other buttons disappear temporarily?
-
-            //progress the game state
-            //ProgressGameplay(/*pass datetime from the users DB entry*/,/*pass stock symbol from users DB entry*/);
 
         }
 
-        public async Task<IActionResult> OnPostSell()
-        {
-            // have user select how much stock to sell
-            //maybe have a seperate div with a input box and button that appears when sell is clicked and make the other buttons disappear temporarily?
+        //public async Task<IActionResult> OnPostBuy()
+        //{
+        //    //have user indicate how much stock to buy
+        //    //maybe have a seperate div with a input box and button that appears when buy is clicked and make the other buttons disappear temporarily?
 
-            //add dollar amount to users account
+        //    //progress the game state
+        //    //ProgressGameplay(/*pass datetime from the users DB entry*/,/*pass stock symbol from users DB entry*/);
 
-            //progress the game state
-            //ProgressGameplay(/*pass datetime from the users DB entry*/,/*pass stock symbol from users DB entry*/);
+        //}
 
-        }
+        //public async Task<IActionResult> OnPostSell()
+        //{
+        //    // have user select how much stock to sell
+        //    //maybe have a seperate div with a input box and button that appears when sell is clicked and make the other buttons disappear temporarily?
 
-        public async Task<IActionResult> OnPostHold()
-        {
-            //do nothing and progress the game state
-            //ProgressGameplay(/*pass datetime from the users DB entry*/,/*pass stock symbol from users DB entry*/);
-        }
+        //    //add dollar amount to users account
 
-        public async Task<IActionResult> OnPostQuit()
-        {
-            //quit game completely
-                //sell all stock the user has and add the money to bank account
-                //show ending results
-        }
+        //    //progress the game state
+        //    //ProgressGameplay(/*pass datetime from the users DB entry*/,/*pass stock symbol from users DB entry*/);
+
+        //}
+
+        //public async Task<IActionResult> OnPostHold()
+        //{
+        //    //do nothing and progress the game state
+        //    //ProgressGameplay(/*pass datetime from the users DB entry*/,/*pass stock symbol from users DB entry*/);
+        //}
+
+        //public async Task<IActionResult> OnPostQuit()
+        //{
+        //    //quit game completely
+        //        //sell all stock the user has and add the money to bank account
+        //        //show ending results
+        //}
 
     }
 }
