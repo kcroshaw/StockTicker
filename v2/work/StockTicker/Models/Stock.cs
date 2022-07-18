148651707802a6dfb5d0fe07f6bc5a4dcf287e6c
CS-EDU921 <84093555+CS-EDU921@users.noreply.github.com>
2022-07-17 21:53:19 -0600

    V2_ADD

---
+++ v2/work/StockTicker/Models/Stock.cs
@@ -0,0 +1,10 @@
+﻿namespace StockTicker.Models
+{
+    public class Stock
+    {
+        public string Symbol { get; set; }
+        public DateTime From { get; set; }
+        public double Open { get; set; }
+        public double Close { get; set; }
+    }
+}
